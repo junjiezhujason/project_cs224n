607e318d346a88126813a3f876f15f2aaaf20008
junjiezhujason <junjie.zhu.jason@gmail.com>
2017-03-17 23:16:29 -0700

    config 001

--- run_on_azure.sh
+++ run_on_azure.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-
-NAME=exp0
+NAME=exp001
 MDIR=run/${NAME}
 LDIR=${MDIR}/log
 TDIR=${MDIR}/train
@@ -16,11 +15,14 @@ rm -r data/tmp-squad-train
 mkdir -p ${MDIR} 
 
 # run python script
-python code/train.py --model=baseline \
-                     --decoder_type=naive \
+python code/train.py --log_dir=${LDIR} \
+                     --train_dir=${TDIR} \
                      --data_size=full \
+                     --model=baseline \
+                     --decoder_type=naive \
                      --batch_size=32 \
                      --learning_rate=0.003 \
                      --state_size=50 \
-                     --log_dir=${LDIR} \
-                     --train_dir=${TDIR} 
+                     --gpu_id=0 \
+                     --gpu_fraction=0.33 \
+
