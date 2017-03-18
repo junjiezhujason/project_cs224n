495f514c59c7f903164c87436fe5432ee92e22c4
junjiezhujason <junjie.zhu.jason@gmail.com>
2017-03-18 00:12:53 -0700

    modified train_dir

--- exec_gpu.sh
+++ exec_gpu.sh
@@ -7,22 +7,22 @@ TDIR=${MDIR}/train
 
 source .env/bin/activate
 
-rm -r data/tmp-squad-train 
+# rm -r data/tmp-squad-train 
 # rm -r train/
 # rm -r log/
 
 # create folder for experiment
 mkdir -p ${MDIR} 
 
-# run python script
+export CUDA_VISIBLE_DEVICES=0
 python code/train.py --log_dir=${LDIR} \
                      --train_dir=${TDIR} \
                      --data_size=full \
                      --model=baseline \
                      --decoder_type=naive \
+                     --epochs=15 \
                      --batch_size=32 \
-                     --learning_rate=0.003 \
+                     --learning_rate=0.001 \
                      --state_size=50 \
-                     --gpu_id=0 \
-                     --gpu_fraction=0.33 \
+                     --gpu_fraction=0.5 \
 
