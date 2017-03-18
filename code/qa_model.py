f5a1bf168febe41903ea1aa03b4765e82c942f98
Yifei Feng <yifeif@google.com>
2017-03-18 12:10:59 -0700

    Make baseline decode layer using lstm for start index as well.

--- code/qa_model.py
+++ code/qa_model.py
@@ -399,6 +399,10 @@ class QASystem(object):
         return a_s, a_e
 
     def lstm_decoder(self, H_in, d, con_len):
+        """
+        Args:
+        con_len is self.config.max_context_length
+        """
         logging.info("="*10+"LSTM Decoder"+"="*10)
         xavier_init = tf.contrib.layers.xavier_initializer()
         zero_init = tf.constant_initializer(0)
@@ -406,24 +410,22 @@ class QASystem(object):
         logging.info("d:"+str(d))
         logging.info("con_len:"+str(con_len))
 
-        with tf.variable_scope("lstm_decoder"):
-            Wp_s = tf.get_variable('Wp_s', shape=(d, ), initializer=xavier_init, dtype=tf.float64)
-            Wp_e = tf.get_variable('Wp_e', shape=(d, ), initializer=xavier_init, dtype=tf.float64)
-            # b_s  = tf.get_variable('b_s', shape=(), initializer=zero_init, dtype=tf.float64)
-            # b_e  = tf.get_variable('b_e', shape=(), initializer=zero_init, dtype=tf.float64)
+        def lstm_decoder_cell(H_in, d):
+            """Helper function to create a lstm decoder."""
+            Wp = tf.get_variable('Wp', shape=(d, ), initializer=xavier_init, dtype=tf.float64)
+            # implement a lstm with H_in as input and H_out where H_out will be used for the end position prediction
+            cell = tf.nn.rnn_cell.LSTMCell(num_units=d, state_is_tuple=True)
+            H_out, _ = tf.nn.dynamic_rnn(cell=cell,
+                                        inputs=H_in,
+                                        sequence_length=self.context_length_placeholder,
+                                        dtype=tf.float64)
+            a  = tf.reshape(tf.matmul(tf.reshape(H_out, [-1, d]), tf.expand_dims(Wp, 1)), [-1, con_len]) #  + b_s
+            return a
 
-            with tf.variable_scope('answer_start'):
-                a_s  = tf.reshape(tf.matmul(tf.reshape(H_in, [-1, d]), tf.expand_dims(Wp_s, 1)), [-1, con_len]) #  + b_s
-
-            with tf.variable_scope('answer_end'):
-                # implement a lstm with H_in as input and H_out where H_out will be used for the end position prediction
-                cell = tf.nn.rnn_cell.LSTMCell(num_units=d, state_is_tuple=True)
-                context_len = self.context_length_placeholder
-                H_out, _ = tf.nn.dynamic_rnn(cell=cell,
-                                             inputs=H_in,
-                                             sequence_length=context_len,
-                                             dtype=tf.float64)
-                a_e  = tf.reshape(tf.matmul(tf.reshape(H_out, [-1, d]), tf.expand_dims(Wp_e, 1)), [-1, con_len]) #  + b_s
+        with tf.variable_scope("answer_start_decoder"):
+            a_s = lstm_decoder_cell(H_in, d)
+        with tf.variable_scope("answer_end_decoder"):
+            a_e = lstm_decoder_cell(H_in, d)
         return a_s, a_e
 
     def naive_decoder(self, H_r, simple=True):
