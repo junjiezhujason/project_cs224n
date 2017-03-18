012c1e879b382dec9527751b7a19c906dfb55fef
junjiezhujason <junjie.zhu.jason@gmail.com>
2017-03-17 22:39:59 -0700

    modified run_on_azure.sh

--- run_on_azure.sh
+++ run_on_azure.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
 
+
+NAME=exp0
+MDIR=run/${NAME}
+LDIR=${MDIR}/log
+TDIR=${MDIR}/train
+
 source .env/bin/activate
 
 rm -r train/
 rm -r data/tmp-squad-train 
 rm -r log/
+
+# create folder for experiment
+mkdir -p ${MDIR} 
+
+# run python script
 python code/train.py --model=baseline \
                      --decoder_type=naive \
-                     --max_context_length=300 \
-                     --max_question_length=60 \
                      --data_size=full \
                      --batch_size=32 \
                      --learning_rate=0.03 \
-                     --state_size=100
+                     --state_size=50 \ 
+                     --log_dir=${LDIR} \
+                     --train_dir=${TDIR} 
