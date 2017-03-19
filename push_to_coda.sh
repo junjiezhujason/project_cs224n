6b99bd7bf01506fd76d4eee352db060a466bab1f
junjiezhujason <junjie.zhu.jason@gmail.com>
2017-03-19 15:26:10 -0700

    updated push_to_coda for matchlstm

--- push_to_coda.sh
+++ push_to_coda.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-EXP=exp005
+EXP=exp009
 
-# cl work main::cs224n-jyj
 # cl upload code
 # cl upload data 
-# cl upload run/$EXP
+# cl upload $EXP
 
-cl run --name run-predict --request-docker-image sckoo/cs224n-squad:v4 :code :data :exp005 dev.json:0x4870af2556994b0687a1927fcec66392 'python code/qa_answer.py --dev_path dev.json --config_path=exp005/log/flags.json --train_dir=exp005/train'
+# cl run --name run-predict-pointer --request-docker-image sckoo/cs224n-squad:v4 :code :data :exp009 dev.json:0x4870af2556994b0687a1927fcec66392 'python code/qa_answer.py --dev_path dev.json --config_path=exp009/log/flags.json --train_dir=exp009/train'
 
