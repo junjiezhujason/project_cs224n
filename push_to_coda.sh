faeeba39049fed801778898f578758ed490d7925
junjiezhujason <junjie.zhu.jason@gmail.com>
2017-03-20 00:06:40 -0700

    push to coda modified, do not sh it

--- push_to_coda.sh
+++ push_to_coda.sh
@@ -6,5 +6,5 @@ EXP=exp009
 # cl upload data 
 # cl upload $EXP
 
-# cl run --name run-predict-pointer --request-docker-image sckoo/cs224n-squad:v4 :code :data :exp009 dev.json:0x4870af2556994b0687a1927fcec66392 'python code/qa_answer.py --dev_path dev.json --config_path=exp009/log/flags.json --train_dir=exp009/train'
+cl run --name run-predict --request-docker-image sckoo/cs224n-squad:v4 :code :data :exp009 dev.json:0x4870af2556994b0687a1927fcec66392 'python code/qa_answer.py --dev_path dev.json --config_path=exp009/log/flags.json --train_dir=exp009/train'
 
